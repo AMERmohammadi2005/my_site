274f87ee41afff0e58017591a71fa7686e2c17ea
amer <mohammadiamer84@gmail.com>
2025-11-18 09:02:53 +0330

    adding some abillity and and change timplate of website
     i changed some parts of blog-home and blog-single and add ability like when you go to single post page counted view will added one and just posts will shows published_date is equal or less than time.now

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from blog.views import *
 app_name = "blog"
 
 urlpatterns = [
-    path("" , blog_view ,name='blog'),
-    path("single/" , blog_single_view ,name='blog_single'),
-    path("post-<int:pid>" ,test_view , name ='test'  )
+    path("" , blog_view ,name='index'),
+    path("<int:pid>" , blog_single_view ,name='single'),
+    #path("post-<int:pid>" ,test_view , name ='test'  )
 
 ]
